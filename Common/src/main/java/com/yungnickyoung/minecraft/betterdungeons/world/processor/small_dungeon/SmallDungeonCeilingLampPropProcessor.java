550ce9ccad482b1d5a73b47fb9524e1ad423937a
Nick Young <yungnickyoung@gmail.com>
2022-06-28 21:32:41 -0400

    Remove soul lanterns from small dungeons. Add /locate anchors to all dungeons except spider. Dep YAPI 3.4.0.

--- Common/src/main/java/com/yungnickyoung/minecraft/betterdungeons/world/processor/small_dungeon/SmallDungeonCeilingLampPropProcessor.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/betterdungeons/world/processor/small_dungeon/SmallDungeonCeilingLampPropProcessor.java
@@ -6,7 +6,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessor;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorType;
@@ -28,12 +27,11 @@ public class SmallDungeonCeilingLampPropProcessor extends StructureProcessor {
 
             // Choose lamp prop
             float f = random.nextFloat();
-            if (f < 0.1f) // Soul lantern
-                blockInfoGlobal = new StructureTemplate.StructureBlockInfo(blockInfoGlobal.pos, Blocks.SOUL_LANTERN.defaultBlockState().setValue(BlockStateProperties.HANGING, true), blockInfoGlobal.nbt);
-            else if (f < 0.625f) // Chain
+            if (f < 0.625f) { // Chain
                 blockInfoGlobal = new StructureTemplate.StructureBlockInfo(blockInfoGlobal.pos, Blocks.CHAIN.defaultBlockState(), blockInfoGlobal.nbt);
-            else // None
+            } else { // None
                 blockInfoGlobal = new StructureTemplate.StructureBlockInfo(blockInfoGlobal.pos, Blocks.CAVE_AIR.defaultBlockState(), blockInfoGlobal.nbt);
+            }
         }
 
         return blockInfoGlobal;
