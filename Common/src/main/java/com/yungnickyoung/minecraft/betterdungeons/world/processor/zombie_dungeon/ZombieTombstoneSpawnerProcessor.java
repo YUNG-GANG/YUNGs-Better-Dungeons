c752773c94e7a27702dcbfaccccc8c5ae77c230f
Nick Young <yungnickyoung@gmail.com>
2022-06-28 21:45:11 -0400

    Fix skeleton spawners in zombie dungeons not spawning skeletons w/ swords for first few spawns.

--- Common/src/main/java/com/yungnickyoung/minecraft/betterdungeons/world/processor/zombie_dungeon/ZombieTombstoneSpawnerProcessor.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/betterdungeons/world/processor/zombie_dungeon/ZombieTombstoneSpawnerProcessor.java
@@ -6,21 +6,16 @@ import com.yungnickyoung.minecraft.yungsapi.world.spawner.MobSpawnerData;
 import net.minecraft.Util;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.FloatTag;
 import net.minecraft.nbt.ListTag;
-import net.minecraft.util.random.SimpleWeightedRandomList;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.SpawnData;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessor;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorType;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
-
-import java.util.Optional;
 
 /**
  * Sets mob spawners to spawn skeletons w/ swords.
@@ -39,24 +34,14 @@ public class ZombieTombstoneSpawnerProcessor extends StructureProcessor {
         if (blockInfoGlobal.state.getBlock() == Blocks.BLACK_STAINED_GLASS) {
             // Create spawner & populate with data
             MobSpawnerData spawner = MobSpawnerData.builder()
-                    .spawnPotentials(SimpleWeightedRandomList.single(new SpawnData(
-                            Util.make(new CompoundTag(), (compoundTag) -> {
-                                compoundTag.putString("id", "minecraft:skeleton");
-                                ListTag handDropChances = new ListTag();
-                                handDropChances.add(FloatTag.valueOf(.2f));
-                                handDropChances.add(FloatTag.valueOf(0f));
-                                compoundTag.put("HandDropChances", handDropChances);
-                                ListTag handItems = new ListTag();
-                                ItemStack itemStack = new ItemStack(Items.IRON_SWORD);
-                                CompoundTag ironSwordNBT = new CompoundTag();
-                                itemStack.save(ironSwordNBT);
-                                handItems.add(ironSwordNBT);
-                                handItems.add(new CompoundTag());
-                                compoundTag.put("HandItems", handItems);
-                            }),
-                            Optional.empty())))
                     .setEntityType(EntityType.SKELETON)
                     .build();
+            spawner.nextSpawnData.getEntityToSpawn().put("HandItems", Util.make(new ListTag(), (handItemsTag) -> {
+                ItemStack itemStack = new ItemStack(Items.IRON_SWORD);
+                CompoundTag ironSwordNBT = new CompoundTag();
+                itemStack.save(ironSwordNBT);
+                handItemsTag.add(ironSwordNBT);
+            }));
             CompoundTag nbt = spawner.save();
             blockInfoGlobal = new StructureTemplate.StructureBlockInfo(blockInfoGlobal.pos, Blocks.SPAWNER.defaultBlockState(), nbt);
         }
