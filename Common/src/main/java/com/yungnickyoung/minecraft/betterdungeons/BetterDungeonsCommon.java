e31da2aa8acd358d94dd4dc1e0b437f1feaecf01
williambl <william@williambl.com>
2026-09-10 14:01:52 +0100

    Switch to using new YUNG's API LocateReplacer API
    YUNG-GANG/YUNGs-Better-Desert-Temples#51

--- Common/src/main/java/com/yungnickyoung/minecraft/betterdungeons/BetterDungeonsCommon.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/betterdungeons/BetterDungeonsCommon.java
@@ -3,6 +3,12 @@ package com.yungnickyoung.minecraft.betterdungeons;
 import com.yungnickyoung.minecraft.betterdungeons.module.ConfigModule;
 import com.yungnickyoung.minecraft.betterdungeons.services.Services;
 import com.yungnickyoung.minecraft.yungsapi.api.YungAutoRegister;
+import com.yungnickyoung.minecraft.yungsapi.api.world.structure.locate.LocateReplacer;
+import com.yungnickyoung.minecraft.yungsapi.world.structure.locate.LocateReplacerImpl;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -17,5 +23,10 @@ public class BetterDungeonsCommon {
     public static void init() {
         YungAutoRegister.scanPackageForAnnotations("com.yungnickyoung.minecraft.betterdungeons.module");
         Services.MODULES.loadModules();
+
+        LocateReplacer.registerRemoval(
+                ResourceKey.create(Registries.STRUCTURE, Identifier.fromNamespaceAndPath(MOD_ID, "small_nether_dungeon")),
+                Component.translatable("command.betterdungeons.locate.small_nether_dungeons"),
+                () -> !CONFIG.smallNetherDungeons.enabled);
     }
 }
